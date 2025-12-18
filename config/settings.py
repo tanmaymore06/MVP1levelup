99695ab9a7cd65d904becffe6767769450117c4b
tanmaymore06 <tanmaymore900@gmail.com>
2025-12-18 12:48:27 +0530

    feat: add core apps
    Structure for quests, content pages, and user progress tracking.

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'quests',
+    'content',
+    'progress',
 ]
 
 MIDDLEWARE = [
