96679bf587980654c7851e18d00d485daad0019e
tanmaymore06 <tanmaymore900@gmail.com>
2026-03-04 13:29:03 +0530

    feat: evaluate_streak_if_needed(user)
    This function evaluates Streak and Freeze Streak counts. It must be called only when player do not want to complete any more sessions today.

--- progress/services/session_execution.py
+++ progress/services/session_execution.py
@@ -1,4 +1,5 @@
 from django.utils import timezone
+from datetime import timedelta
 from progress.models import *
 from quests.services.progression import get_session
 
@@ -31,3 +32,68 @@ def complete_session(user):
     )
 
     return session_completion
+
+
+
+
+def evaluate_streak_if_needed(user):
+    today = timezone.localdate()
+
+    streak_state, created = UserStreakState.objects.get_or_create(
+        user=user,
+        defaults={
+            "streak": 0,
+            "freeze_streak": 0,
+            "consecutive_zero_session_days": 0,
+            "last_evaluated_date": today - timedelta(days=1),  # Start evaluation from yesterday.
+        }
+    )
+
+    # If already evaluated today, do nothing
+    if streak_state.last_evaluated_date == today:
+        return streak_state
+
+    # Start evaluating from the next day after last evaluation
+    current_date = streak_state.last_evaluated_date + timedelta(days=1)
+
+    while current_date <= today:
+
+        session_count = SessionCompletion.objects.filter(
+            user=user,
+            calendar_date=current_date
+        ).count()
+
+        # Case A: User completed at least one session
+        if session_count >= 1:
+            streak_state.streak += 1
+            streak_state.consecutive_zero_session_days = 0
+
+            # Freeze increment only once per day
+            if session_count > 2:
+                streak_state.freeze_streak = min(
+                    streak_state.freeze_streak + 1,
+                    5
+                )
+            print("Evaluating date:", current_date)
+            print("Session count:", session_count)
+
+        # Case B: User completed zero sessions
+        else:
+            streak_state.consecutive_zero_session_days += 1
+
+            if streak_state.consecutive_zero_session_days <= 2:
+                # Use freeze if available
+                if streak_state.freeze_streak > 0:
+                    streak_state.freeze_streak -= 1
+            else:
+                # Third consecutive missed day resets streak
+                streak_state.streak = 0
+                streak_state.consecutive_zero_session_days = 0
+
+        current_date += timedelta(days=1)
+
+    # Mark evaluation complete
+    streak_state.last_evaluated_date = today
+    streak_state.save()
+
+    return streak_state
