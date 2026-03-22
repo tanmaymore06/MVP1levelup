818c7131bc1b192dc6f6dd298739426f1dab85ce
tanmaymore06 <tanmaymore900@gmail.com>
2026-03-22 11:25:45 +0530

    feat: add auth URL routes

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import RegisterView, LoginView, LogoutView
+
+urlpatterns = [
+    path("auth/register/", RegisterView.as_view(), name="auth-register"),
+    path("auth/login/", LoginView.as_view(), name="auth-login"),
+    path("auth/logout/", LogoutView.as_view(), name="auth-logout"),
+]
